c021826a2f52b3d84fad3cf9e69e3704583b72de
JesusJr <Jesusujr101@gmail.com>
2023-12-06 19:34:08 -0500

    adding code to login/landing/signup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
@@ -31,6 +32,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static('img'));
 
 app.use(routes);
 
